4c32e753c3da7f8b2b1549ff3a69e56fc1183963
Ramir Franco L. Castro <ramirfrancocastro@gmail.com>
2024-05-07 21:20:28 +0800

    table changes

--- Styles/AppStyles.js
+++ Styles/AppStyles.js
@@ -34,37 +34,36 @@ const styles = StyleSheet.create({
   },
   table: {
     marginBottom: 20,
-    borderWidth: 1,
-    borderColor: '#ddd',
     borderRadius: 5,
   },
   tableHeader: {
     flexDirection: 'row',
+    color: '#8C8F8F',
     justifyContent: 'space-between',
     backgroundColor: '#101523',
     padding: 12,
-    marginLeft: 10,
   },
   tableRow: {
     flexDirection: 'row', // Enable flexbox
     alignItems: 'center', // Align items vertically within the row
     justifyContent: 'space-between', // Distribute remaining space
     paddingVertical: 10,
-    borderBottomWidth: 1,
-    borderColor: '#ddd',
   },
   tableDataBlinds: {
     width: 100, // Set a fixed width for blinds text
     textAlign: 'right',
     marginRight: 30,
+    color: '#959AB0',
   },
   tableDataTime: {
     flex: 0.7,
     textAlign: 'right',
+    color: '#959AB0',
   },
   tableDataLevel: {
     flex: 0.15,
     textAlign: 'right',
+    color: '#959AB0',
   },
   tableData: {
     color: '#898EA4',
